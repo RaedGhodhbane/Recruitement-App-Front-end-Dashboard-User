0e28dd7df62aa3b4da95afe17087e0fd345edb6b
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-08-29 18:38:27 +0100

    Implement "Contact-us" component

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ContactService } from '../services/contact.service';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact-us',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  allMessages: any[] = [];
+  messages: any[] = [];
+  messageForm!: FormGroup;
+  errorMessage= '';
+  message: any;
+  admin: any;
 
-  constructor() { }
+  userConnect: any;
+  constructor(private fb:FormBuilder,private contactService: ContactService,
+    private activateRoute: ActivatedRoute, private cdr: ChangeDetectorRef,) { }
 
   ngOnInit(): void {
+    this.messageForm = this.fb.group({
+      name: ['',Validators.required],
+      subject:['',Validators.required],
+      email:['',Validators.required],
+      phone:[,Validators.required],
+      message:['', Validators.required]
+    })
+    this.userConnect = JSON.parse(localStorage.getItem('user')!)
   }
 
+  sendMessageContact() {
+  if (this.messageForm.invalid) {
+    this.errorMessage = "Veuillez remplir tous les champs correctement.";
+    return ;
+  }
+
+    this.userConnect = JSON.parse(localStorage.getItem('user')!);
+
+    this.contactService.sendMessageByUser(this.messageForm.value,this.userConnect.id).subscribe({
+    next: (response) => {
+      this.message = response
+      console.log('Message envoyé avec succès', this.message);
+    },
+    error: (error) => {
+      console.log('Erreur lors de l`envoie du message', error);
+      this.errorMessage= 'Une erreur est survenue. Veuillez réessayer.';
+    }
+  });
+  this.messageForm.reset();
 }
+  }
