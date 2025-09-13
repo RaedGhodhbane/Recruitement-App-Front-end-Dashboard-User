1c5d5a48d0c8046c7eb96c3d5d62f04ebb77053f
Raed.Ghodhbane <ghodhbane.raed@gmail.com>
2025-09-13 15:43:50 +0200

    Add jobDetail routerLink and pagination to "Saved Jobs" component

--- src/candidate/candidates-dashboard/saved-jobs-candidate/saved-jobs-candidate.component.ts
+++ src/candidate/candidates-dashboard/saved-jobs-candidate/saved-jobs-candidate.component.ts
@@ -8,42 +8,79 @@ import { SavedJobService } from 'src/app/services/saved-job.service';
   styleUrls: ['./saved-jobs-candidate.component.css']
 })
 export class SavedJobsCandidateComponent implements OnInit {
-educations:any
-userConnect:any
-favorites:any[] = [];
-  constructor(private educationService:EducationService, private savedJobService: SavedJobService) { }
+  educations: any;
+  userConnect: any;
+
+  favorites: any[] = [];
+  pagedFavorites: any[] = [];
+
+  page: number = 1;
+  pageSize: number = 5;
+  totalPages: number = 0;
+
+  constructor(
+    private educationService: EducationService,
+    private savedJobService: SavedJobService
+  ) {}
 
   ngOnInit(): void {
     this.userConnect = JSON.parse(localStorage.getItem('user')!);
     console.log("userConnect au chargement :", this.userConnect);
+
     this.getEducationsCandidate();
     this.loadSavedJobs();
-
-
   }
-getEducationsCandidate() {
-  this.educationService.getAllEducations().subscribe(
-    (res:any) => {
-          this.educations = res.filter((element:any) => (element.candidate?.id == this.userConnect.id));
 
-      console.log("Tous les educations retournés :", res)
-    }
-  )
-}
+  getEducationsCandidate() {
+    this.educationService.getAllEducations().subscribe(
+      (res: any) => {
+        this.educations = res.filter(
+          (element: any) => element.candidate?.id == this.userConnect.id
+        );
+        console.log("Tous les educations retournés :", res);
+      }
+    );
+  }
 
   loadSavedJobs() {
     this.savedJobService.getSavedJobs(this.userConnect.id).subscribe(
       (res: any[]) => {
         this.favorites = res;
+        this.totalPages = Math.ceil(this.favorites.length / this.pageSize);
+        this.updatePage();
         console.log("Jobs sauvegardés :", this.favorites);
       },
       (err) => console.error(err)
     );
   }
 
+  updatePage() {
+    const startIndex = (this.page - 1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    this.pagedFavorites = this.favorites.slice(startIndex, endIndex);
+  }
+
+  goToPage(p: number) {
+    if (p >= 1 && p <= this.totalPages) {
+      this.page = p;
+      this.updatePage();
+    }
+  }
+
+  nextPage() {
+    this.goToPage(this.page + 1);
+  }
+
+  prevPage() {
+    this.goToPage(this.page - 1);
+  }
+
   removeFromFavourites(jobId: number) {
     this.savedJobService.removeSavedJob(jobId).subscribe(() => {
       this.favorites = this.favorites.filter(job => job.id !== jobId);
+      this.totalPages = Math.ceil(this.favorites.length / this.pageSize);
+      if (this.page > this.totalPages) this.page = this.totalPages || 1;
+      this.updatePage();
     });
   }
 }
